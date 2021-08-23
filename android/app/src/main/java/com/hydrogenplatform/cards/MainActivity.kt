67fea93111382c06d95f7236e1ecb270a0c3a824
Vitali Bahdzevich <vitali.bahdzevich@nbe-221-71b.local>
2021-08-23 12:55:17 +0300

    android

---
+++ android/app/src/main/java/com/hydrogenplatform/cards/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.hydrogenplatform.cards
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hydrogenplatform.card_modules.Balance
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var cardIssuance: Balance
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        cardIssuance = findViewById(R.id.issuance)
+
+        val publicKey="your publicKey"
+        val appKey="your appKey"
+
+        cardIssuance.showComponent(publicKey,appKey, "")
+    }
+}
--- react-native/README.md
+++ react-native/README.md
@@ -3,3 +3,5 @@
 ### 2. navigate to `ios` folder and run `pod install`
 
 ### 3. open `App.js` file and fill the following fields `publicKey=""` `appToken=""`
+
+## This sample uses `SANDBOX` environment
